5c928ebbeb8ff2948d6d6cc75ec1e0fd19ce4c68
Helmi Hernandez <helmidh@hotmail.com>
2024-06-02 17:15:48 -0400

    starting 2.6

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -12,6 +12,11 @@ public class MovingSphere : MonoBehaviour
     [SerializeField, Range(0f, 10f)]
     float jumpHeight = 2f;
 
+    [SerializeField, Range(0, 5)]
+    int maxAirJumps = 0;
+
+    int jumpPhase;
+
     Vector3 velocity, desiredVelocity;
 
     Rigidbody body;
@@ -37,7 +42,6 @@ public class MovingSphere : MonoBehaviour
 
     void FixedUpdate()
     {
-        velocity = body.velocity;
         float maxSpeedChange = maxAcceleration * Time.deltaTime;
         velocity.x =
             Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
@@ -53,13 +57,22 @@ public class MovingSphere : MonoBehaviour
         onGround = false;
     }
 
-    void Jump()
+    void UpdateState ()
     {
+        velocity = body.velocity;
         if (onGround)
         {
+            jumpPhase = 0;
+        }
+    }
+
+    void Jump()
+    {
+        if (onGround || jumpPhase < maxAirJumps)
+        {
+            jumpPhase += 1;
             velocity.y += Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
         }
-
     }
 
     void OnCollisionEnter(Collision collision)
