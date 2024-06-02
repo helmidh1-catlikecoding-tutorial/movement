32fe7436f73177def6bf3c2aa1fd9ff52daf14ed
Helmi Hernandez <helmidh@hotmail.com>
2024-06-02 17:12:15 -0400

    starting 2.5

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -9,9 +9,15 @@ public class MovingSphere : MonoBehaviour
     [SerializeField, Range(0f, 100f)]
     float maxAcceleration = 10f;
 
+    [SerializeField, Range(0f, 10f)]
+    float jumpHeight = 2f;
+
     Vector3 velocity, desiredVelocity;
 
     Rigidbody body;
+
+    bool desiredJump;
+    bool onGround;
 
     void Awake()
     {
@@ -26,6 +32,7 @@ public class MovingSphere : MonoBehaviour
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
         desiredVelocity =
             new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        desiredJump |= Input.GetButtonDown("Jump");
     }
 
     void FixedUpdate()
@@ -36,6 +43,41 @@ public class MovingSphere : MonoBehaviour
             Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
         velocity.z =
             Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        if (desiredJump)
+        {
+            desiredJump = false;
+            Jump();
+        }
         body.velocity = velocity;
+        onGround = false;
+    }
+
+    void Jump()
+    {
+        if (onGround)
+        {
+            velocity.y += Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    void EvaluateCollision (Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            Vector3 normal = collision.GetContact(i).normal;
+            onGround |= normal.y >= 0.9f;
+        }
     }
 }
